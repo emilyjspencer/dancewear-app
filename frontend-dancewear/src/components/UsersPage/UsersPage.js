9d3cda6d2bd8c58614aa468486fdf052a06fe2b6
Emily <emilyspencer89@hotmail.co.uk>
2023-06-12 22:25:31 +0100

    Text showing

--- frontend-dancewear/src/components/UsersPage/UsersPage.js
+++ frontend-dancewear/src/components/UsersPage/UsersPage.js
@@ -28,15 +28,14 @@ return (
           <ul>
             {users.map(user => (
               <>
-                <li key={user.id}>
-                </li>
+                <li key={user.id}></li>
                 <Cards
                   imgSrc=""
                   imgAlt="User placeholder"
                   text1={`First name: ${user.first_name}`}
                   text2={`Last name: ${user.last_name}`}
-                  text3={`Job location: ${user.email}`}
-                  text4={`Contract duration: ${user.phone_number}`}
+                  text3={`Email: ${user.email}`}
+                  text4={`Phone number: ${user.phone_number}`}
                 />
               
               </>
