3ff83d2fdac5fa95469eb1c2cdba9f3829d0dbae
Emily <emilyspencer89@hotmail.co.uk>
2023-06-13 14:13:01 +0100

    Fleshes out Product service and Product controller

--- dancewear/src/main/java/com/example/dancewear/controller/ProductController.java
+++ dancewear/src/main/java/com/example/dancewear/controller/ProductController.java
@@ -1,14 +1,23 @@
 package com.example.dancewear.controller;
 
 
+import com.example.dancewear.entity.Blog;
 import com.example.dancewear.entity.Product;
+import com.example.dancewear.exceptions.BlogAlreadyExistsException;
+import com.example.dancewear.exceptions.BlogNotFoundException;
+import com.example.dancewear.exceptions.ProductAlreadyExistsException;
+import com.example.dancewear.exceptions.ProductNotFoundException;
+import com.example.dancewear.service.BlogService;
 import com.example.dancewear.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,14 +25,48 @@ import java.util.List;
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private static final Logger log = LoggerFactory.getLogger("ProductController.class");
     @Autowired
     private ProductService productService;
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public List<Product> getAll() {
+        log.info("ProductController - get all products");
         return productService.getAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody Product product) throws ProductAlreadyExistsException {
+        Product newProduct = productService.addProduct(product);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newProduct.getId()).toUri();
+        log.info("Add product to database");
+        return ResponseEntity.created(location).build();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable int id) throws ProductNotFoundException {
+        log.info("Find product by id " + id);
+        return ResponseEntity.status(HttpStatus.OK).body(productService.findProductById(id));
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable int id) throws ProductNotFoundException{
+        productService.deleteProductById(id);
+        log.info("Delete product with id of: " + id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PutMapping
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) throws ProductNotFoundException{
+        log.info("Update product]");
+        return ResponseEntity.status(HttpStatus.OK).body(productService.updateProduct(product));
+    }
 }
+
+
 
--- dancewear/src/main/java/com/example/dancewear/service/ProductService.java
+++ dancewear/src/main/java/com/example/dancewear/service/ProductService.java
@@ -1,24 +1,72 @@
 package com.example.dancewear.service;
 
+import com.example.dancewear.dao.BlogRepository;
 import com.example.dancewear.dao.ProductRepository;
+import com.example.dancewear.entity.Blog;
 import com.example.dancewear.entity.Product;
+import com.example.dancewear.exceptions.BlogAlreadyExistsException;
+import com.example.dancewear.exceptions.BlogNotFoundException;
+import com.example.dancewear.exceptions.ProductAlreadyExistsException;
+import com.example.dancewear.exceptions.ProductNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     private ProductRepository productRepository;
 
-    @Autowired
+    private static final Logger log = LoggerFactory.getLogger("ProductService.class");
+
     public ProductService(ProductRepository productRepository) {
-        super();
+
         this.productRepository = productRepository;
     }
 
     public List<Product> getAll() {
+        log.info("ProductService - get all");
         return productRepository.findAll();
+    }
+
+    public void deleteProductById(int id) throws ProductNotFoundException {
+        if(!productRepository.existsById(id)){
+            log.warn("Unable to delete product with id of: " + id);
+            throw new ProductNotFoundException("Id: " + id +" doesn't exist");
+        }
+        log.info("Delete product with id of: " + id);
+        productRepository.deleteById(id);
+    }
+
+    public Product addProduct(Product product) throws ProductAlreadyExistsException {
+        if (!productRepository.existsById(product.getId())) {
+            log.info("Add product to database");
+            return productRepository.save(product);
+        }
+        log.warn("Unable to add product, id in use");
+        throw new ProductAlreadyExistsException("Product with Id: " + product.getId() + " already exists");
+    }
+
+    public Product updateProduct(Product product) throws ProductNotFoundException {
+        if (productRepository.existsById(product.getId())) {
+            log.info("Update product");
+            return productRepository.save(product);
+        }
+        log.warn("Unable to update product, no id found");
+        throw new ProductNotFoundException("No product exists with id: " + product.getId());
+    }
+
+    public Product findProductById(int id) throws ProductNotFoundException {
+        Optional<Product> fetchedProduct = productRepository.findById(id);
+        if (fetchedProduct.isPresent()) {
+            log.info("Return product with id of: " + id);
+            return fetchedProduct.get();
+        }
+        log.warn("Unable to get product - product not not in database");
+        throw new ProductNotFoundException("No product exists with id: " + id);
     }
 }
