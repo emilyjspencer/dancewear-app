0de0f290f2284906d92bed4f733b418bf6f972ba
Emily <emilyspencer89@hotmail.co.uk>
2023-06-13 14:26:43 +0100

    Adds CRUD functionality to Reviews Service and ReviewsControler

--- dancewear/src/main/java/com/example/dancewear/controller/ReviewsController.java
+++ dancewear/src/main/java/com/example/dancewear/controller/ReviewsController.java
@@ -1,15 +1,24 @@
 package com.example.dancewear.controller;
 
+import com.example.dancewear.entity.Product;
 import com.example.dancewear.entity.Review;
 import com.example.dancewear.entity.User;
+import com.example.dancewear.exceptions.ProductAlreadyExistsException;
+import com.example.dancewear.exceptions.ProductNotFoundException;
+import com.example.dancewear.exceptions.ReviewAlreadyExistsException;
+import com.example.dancewear.exceptions.ReviewNotFoundException;
+import com.example.dancewear.service.ProductService;
 import com.example.dancewear.service.ReviewsService;
 import com.example.dancewear.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,13 +26,45 @@ import java.util.List;
 @RequestMapping("/api/reviews")
 public class ReviewsController {
 
+    private static final Logger log = LoggerFactory.getLogger("ReviewsController.class");
     @Autowired
     private ReviewsService reviewsService;
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public List<Review> getAll() {
-
+        log.info("ReviewController - get all reviews");
         return reviewsService.getAll();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping
+    public ResponseEntity<Void> addReview(@RequestBody Review review) throws ReviewAlreadyExistsException {
+        Review newReview = reviewsService.addReview(review);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newReview.getId()).toUri();
+        log.info("Add review to database");
+        return ResponseEntity.created(location).build();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/{id}")
+    public ResponseEntity<Review> findReviewById(@PathVariable int id) throws ReviewNotFoundException {
+        log.info("Find review by id " + id);
+        return ResponseEntity.status(HttpStatus.OK).body(reviewsService.findReviewById(id));
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReviewById(@PathVariable int id) throws ReviewNotFoundException{
+        reviewsService.deleteReviewById(id);
+        log.info("Delete review with id of: " + id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PutMapping
+    public ResponseEntity<Review> updateReview(@RequestBody Review review) throws ReviewNotFoundException{
+        log.info("Update review");
+        return ResponseEntity.status(HttpStatus.OK).body(reviewsService.updateReview(review));
     }
 }
--- dancewear/src/main/java/com/example/dancewear/service/ReviewsService.java
+++ dancewear/src/main/java/com/example/dancewear/service/ReviewsService.java
@@ -4,23 +4,69 @@ import com.example.dancewear.dao.ProductRepository;
 import com.example.dancewear.dao.ReviewsRepository;
 import com.example.dancewear.entity.Product;
 import com.example.dancewear.entity.Review;
+import com.example.dancewear.exceptions.ProductAlreadyExistsException;
+import com.example.dancewear.exceptions.ProductNotFoundException;
+import com.example.dancewear.exceptions.ReviewAlreadyExistsException;
+import com.example.dancewear.exceptions.ReviewNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewsService {
 
     private ReviewsRepository reviewsRepository;
 
-    @Autowired
+    private static final Logger log = LoggerFactory.getLogger("ReviewsService.class");
+
     public ReviewsService(ReviewsRepository reviewsRepository) {
-        super();
+
         this.reviewsRepository = reviewsRepository;
     }
 
     public List<Review> getAll() {
+        log.info("ReviewService - get all");
         return reviewsRepository.findAll();
+    }
+
+    public void deleteReviewById(int id) throws ReviewNotFoundException {
+        if(!reviewsRepository.existsById(id)){
+            log.warn("Unable to delete product with id of: " + id);
+            throw new ReviewNotFoundException("Id: " + id +" doesn't exist");
+        }
+        log.info("Delete review with id of: " + id);
+        reviewsRepository.deleteById(id);
+    }
+
+    public Review addReview(Review review) throws ReviewAlreadyExistsException {
+        if (!reviewsRepository.existsById(review.getId())) {
+            log.info("Add review to database");
+            return reviewsRepository.save(review);
+        }
+        log.warn("Unable to add review, id in use");
+        throw new ReviewAlreadyExistsException("Review with Id: " + review.getId() + " already exists");
+    }
+
+    public Review updateReview(Review review) throws ReviewNotFoundException {
+        if (reviewsRepository.existsById(review.getId())) {
+            log.info("Update review");
+            return reviewsRepository.save(review);
+        }
+        log.warn("Unable to update review, no id found");
+        throw new ReviewNotFoundException("No review exists with id: " + review.getId());
+    }
+
+    public Review findReviewById(int id) throws ReviewNotFoundException {
+        Optional<Review> fetchedReview = reviewsRepository.findById(id);
+        if (fetchedReview.isPresent()) {
+            log.info("Return review with id of: " + id);
+            return fetchedReview.get();
+        }
+        log.warn("Unable to get review  - review not not in database");
+        throw new ReviewNotFoundException("No review exists with id: " + id);
     }
 }
