1e276d8edb6caf835c8df09148e8bc28b6f65414
Emily <emilyspencer89@hotmail.co.uk>
2023-06-13 13:55:59 +0100

    Fleshes out blog service and blog controller

--- dancewear/src/main/java/com/example/dancewear/controller/BlogController.java
+++ dancewear/src/main/java/com/example/dancewear/controller/BlogController.java
@@ -2,14 +2,21 @@ package com.example.dancewear.controller;
 
 import com.example.dancewear.entity.Blog;
 import com.example.dancewear.entity.User;
+import com.example.dancewear.exceptions.BlogAlreadyExistsException;
+import com.example.dancewear.exceptions.BlogNotFoundException;
+import com.example.dancewear.exceptions.UserAlreadyExistsException;
+import com.example.dancewear.exceptions.UserNotFoundException;
 import com.example.dancewear.service.BlogService;
 import com.example.dancewear.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,13 +25,45 @@ import java.util.List;
 public class BlogController {
 
 
+        private static final Logger log = LoggerFactory.getLogger("BlogController.class");
         @Autowired
         private BlogService blogService;
 
         @CrossOrigin(origins = "http://localhost:3000")
         @GetMapping
         public List<Blog> getAll() {
+                log.info("blogController - get all blog posts");
+                return blogService.getAll();
+        }
 
-            return blogService.getAll();
+        @CrossOrigin(origins = "http://localhost:3000")
+        @PostMapping
+        public ResponseEntity<Void> addBlogPost(@RequestBody Blog blog) throws BlogAlreadyExistsException {
+                Blog newBlog = blogService.addBlog(blog);
+                URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newBlog.getId()).toUri();
+                log.info("Add blog post to database");
+                return ResponseEntity.created(location).build();
+        }
+
+        @CrossOrigin(origins = "http://localhost:3000")
+        @GetMapping("/{id}")
+        public ResponseEntity<Blog> findBlogById(@PathVariable int id) throws BlogNotFoundException {
+                log.info("Find blog post by id " + id);
+                return ResponseEntity.status(HttpStatus.OK).body(blogService.findBlogById(id));
+        }
+
+        @CrossOrigin(origins = "http://localhost:3000")
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> deleteBlogById(@PathVariable int id) throws BlogNotFoundException{
+                blogService.deleteBlogById(id);
+                log.info("Delete blog post with id of: " + id);
+                return ResponseEntity.status(HttpStatus.OK).build();
+        }
+
+        @CrossOrigin(origins = "http://localhost:3000")
+        @PutMapping
+        public ResponseEntity<Blog> updateBlog(@RequestBody Blog blog) throws BlogNotFoundException{
+                log.info("Update blog post");
+                return ResponseEntity.status(HttpStatus.OK).body(blogService.updateBlog(blog));
         }
 }
--- dancewear/src/main/java/com/example/dancewear/service/BlogService.java
+++ dancewear/src/main/java/com/example/dancewear/service/BlogService.java
@@ -1,25 +1,72 @@
 package com.example.dancewear.service;
 
 import com.example.dancewear.dao.BlogRepository;
+import com.example.dancewear.dao.UserRepository;
 import com.example.dancewear.entity.Blog;
+import com.example.dancewear.entity.User;
+import com.example.dancewear.exceptions.BlogAlreadyExistsException;
+import com.example.dancewear.exceptions.BlogNotFoundException;
+import com.example.dancewear.exceptions.UserAlreadyExistsException;
+import com.example.dancewear.exceptions.UserNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BlogService {
 
     private BlogRepository blogRepository;
 
-    @Autowired
+    private static final Logger log = LoggerFactory.getLogger("BlogService.class");
+
     public BlogService(BlogRepository blogRepository) {
-        super();
+
         this.blogRepository = blogRepository;
     }
 
     public List<Blog> getAll() {
-
+        log.info("BlogService - get all");
         return blogRepository.findAll();
+    }
+
+    public void deleteBlogById(int id) throws BlogNotFoundException {
+        if(!blogRepository.existsById(id)){
+            log.warn("Unable to delete blog post with id of: " + id);
+            throw new BlogNotFoundException("Id: " + id +" doesn't exist");
+        }
+        log.info("Delete blog post with id of: " + id);
+        blogRepository.deleteById(id);
+    }
+
+    public Blog addBlog(Blog blog) throws BlogAlreadyExistsException {
+        if (!blogRepository.existsById(blog.getId())) {
+            log.info("Add Blog post to database");
+            return blogRepository.save(blog);
+        }
+        log.warn("Unable to add blog post, id in use");
+        throw new BlogAlreadyExistsException("Blog post with Id: " + blog.getId() + " already exists");
+    }
+
+    public Blog updateBlog(Blog blog) throws BlogNotFoundException {
+        if (blogRepository.existsById(blog.getId())) {
+            log.info("Update blog");
+            return blogRepository.save(blog);
+        }
+        log.warn("Unable to update blog post, no id found");
+        throw new BlogNotFoundException("No blog post exists with id: " + blog.getId());
+    }
+
+    public Blog findBlogById(int id) throws BlogNotFoundException {
+        Optional<Blog> fetchedBlogPost = blogRepository.findById(id);
+        if (fetchedBlogPost.isPresent()) {
+            log.info("Return blog post with id of: " + id);
+            return fetchedBlogPost.get();
+        }
+        log.warn("Unable to get blog post- blog post not in db");
+        throw new BlogNotFoundException("No blog post exists with id: " + id);
     }
 }
