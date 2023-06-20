468e262cd0914a4f53cacda2d956279fa4dd6ff0
Emily <emilyspencer89@hotmail.co.uk>
2023-06-20 18:42:43 +0100

    Adds JWT dependencies

--- dancewear/src/main/java/com/example/dancewear/jwt/JWTAuthenticationFilter.java
+++ dancewear/src/main/java/com/example/dancewear/jwt/JWTAuthenticationFilter.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+
     }
 }
