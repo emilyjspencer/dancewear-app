300d5d3bb6d2a64ddfaa156a67de4e2c97b98710
Emily <emilyspencer89@hotmail.co.uk>
2023-06-20 22:53:09 +0100

    Gets token from auth header

--- dancewear/src/main/java/com/example/dancewear/jwt/JWTAuthenticationFilter.java
+++ dancewear/src/main/java/com/example/dancewear/jwt/JWTAuthenticationFilter.java
@@ -10,7 +10,21 @@ import java.io.IOException;
 
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest
+                                                httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
 
+
+         String authenticationHeader = httpServletRequest.getHeader("Authorization");
+         String jwtToken;
+        if (authenticationHeader == null | !authenticationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+
+        // extract bearer token from authentication header
+        jwtToken = authenticationHeader.substring(7);
     }
 }
