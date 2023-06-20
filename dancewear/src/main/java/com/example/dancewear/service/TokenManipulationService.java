26fb6b960995b1bc2945a13acc6ff3cbac145faa
Emily <emilyspencer89@hotmail.co.uk>
2023-06-20 23:18:08 +0100

    Methods to get claims and single claim based on token

--- dancewear/src/main/java/com/example/dancewear/service/TokenManipulationService.java
+++ dancewear/src/main/java/com/example/dancewear/service/TokenManipulationService.java
@@ -17,7 +17,8 @@ public class TokenManipulationService {
     private static final String SECRET = "BO2lir6x2mcLV6NWgaJudN4i0WrhKYz8EpjwgYgBC1Fd+OPzTCOdbhZKgFLRHR2v\n";
 
         public String getUsername(String token) {
-            return "testing";
+            return getClaimFromToken(token, Claims::getSubject); // the subject should be the email or username
+            // of the authenticated user
         }
 
     private Key getKey() {
@@ -25,6 +26,25 @@ public class TokenManipulationService {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
 
         return Keys.hmacShaKeyFor(keyBytes); // algorithm
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+        // an algorithm
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
 
