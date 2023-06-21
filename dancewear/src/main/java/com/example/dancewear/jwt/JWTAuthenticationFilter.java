e8243fe92ce2f64b8919a9e5fefe2a5204ae2c0b
Emily <emilyspencer89@hotmail.co.uk>
2023-06-21 15:36:37 +0100

    Adds RequiredArgsConstructor

--- dancewear/src/main/java/com/example/dancewear/jwt/JWTAuthenticationFilter.java
+++ dancewear/src/main/java/com/example/dancewear/jwt/JWTAuthenticationFilter.java
@@ -5,15 +5,19 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
+@Component
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
