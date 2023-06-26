b09e08df82f94757a19e08523c6a8626270ebece
Emily <emilyspencer89@hotmail.co.uk>
2023-06-26 20:49:40 +0100

    Filter blog post by title

--- frontend-dancewear/src/components/Blog/Blog.js
+++ frontend-dancewear/src/components/Blog/Blog.js
@@ -2,9 +2,20 @@
 
 import { useState, useEffect} from 'react';
 import './Blog.css';
+import { Link } from 'react-router-dom';
+import BlogPostCard from '../BlogPostCard/BlogPostCard';
 
 const Blog = () => {
 const [posts, setPosts] = useState([])
+const [post, setPost] = useState("");
+const [searchId, setSearchId] = useState("");
+const [searchBlogPostTitle, setSearchBlogPostTitle] = useState("");
+const [currentIndex, setCurrentIndex] = useState(-1);
+const [currentPost, setCurrentPost] = useState(null);
+const [id, setId] = useState("");
+const [postDetail, setPostDetail] = useState(null);
+const [modalOpen, setModalOpen] = useState(false);
+
 
 const fetchBlogPosts = () => {
   fetch("http://localhost:8080/api/blog")
@@ -22,24 +33,61 @@ useEffect(() => {
 
 console.log(posts);
 
-return (
-  <div>
-    {posts.length > 0 && (
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>
-            {post.title}
-            
-            <br /> 
-            <br />
-          {post.body}
-          <br />
-          {post.date_of_blog_post}</li>
-        ))}
-      </ul>
-    )}
-  </div>
-);
-}
+const setActivePost = (post, index) => {
+  setCurrentPost(post);
+  setCurrentIndex(index);
+};
 
-export default Blog;
+return (
+  <><><>
+      <div className="search-add-container"> 
+        <Link to={"/posts/add"} className="add-blog">Add new blog post</Link>
+      </div>
+      <div>
+              <div className="main">
+                  <div className="search-container">
+                      <div className="">
+                          <input type="number" placeholder="Search .." name="searchId" onChange={(event) => setId(event.target.value)}></input>
+                          <input type="text" placeholder="Search by title" name="searchBlogPostTitle" onChange={(event) => setSearchBlogPostTitle(event.target.value)}></input>
+                          
+                      </div>
+                  </div>
+              </div>
+      </div>
+      {posts.length > 0 && (
+        <ul className="blog-post-list-container">
+            <>
+            {posts.filter((post) => {
+                      return searchId === "" ? post : post.id.toString() === searchId
+                  }).filter((post) => {
+                    return searchBlogPostTitle === "" ? post : post.title.toLowerCase().includes(searchBlogPostTitle)
+                  }).map((post, index) => (
+
+              <><div id="card-modal-button" key={post.id}
+                      onClick={(e) => {
+                        setActivePost(post, index); setPostDetail(post);
+                      } }></div><div id="" key={post.id}
+                        onClick={(e) => {
+                          setActivePost(post, index); setPostDetail(post);
+                           setModalOpen(e);
+                        } }>
+<div className="cards1">
+<BlogPostCard
+                  imgSrc="https://picsum.photos/seed/picsum/200/300"
+                  imgAlt="Post placeholder"
+                  text1={`Title : ${post.title}`}
+                  text2={`Article: ${post.body}`}
+                  text3={`Author: ${post.author_id.name}`}
+                  text4={`Date of article: ${post.date_of_blog_post}`} />
+                  </div>
+                          
+                        </div>
+                     
+                        </>))}
+                        </>
+                        
+                  </ul>
+                  
+      )}</></></>)}
+
+                        export default Blog;
---
+++ frontend-dancewear/src/components/BlogPostCard/BlogPostCard.js
@@ -0,0 +1,45 @@
+import './BlogPostCard.css';
+
+
+import * as React from 'react';
+
+
+const BlogPostCard = ({
+  imgSrc, imgAlt, title, text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, button
+}) => {
+
+
+  return (
+    <div className="card-container">
+      <img src={imgSrc} alt={imgAlt}>
+
+      </img>
+      <br />
+      <div className="text">
+        {title}
+        <br />
+       {text1}
+       <br />
+       {text2}
+       <br />
+       {text3}
+       <br />
+       {text4}
+       <br />
+       {text5}
+       <br />
+       {text6}
+       <br />
+       {text7}
+       <br />
+       {text8}
+       {button}
+      </div>
+    </div>
+  )
+
+}
+ 
+
+
+export default BlogPostCard; 
