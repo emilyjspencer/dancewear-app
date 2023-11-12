a4a56a4c938017afe4dd74ff793218c36dcf68fb
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-11-12 12:41:27 +0000

    Fix(AuthenticationService): Fixes issue with axios in test

--- frontend-dancewear/src/services/AuthenticationService.test.js
+++ frontend-dancewear/src/services/AuthenticationService.test.js
@@ -1,3 +1,41 @@
+import AuthenticationService from './AuthenticationService'; // This overwrites axios methods with jest Mock
+import axios from 'axios';
+jest.mock('axios');
 
+
+describe('AuthenticationService', () => {
+
+  it('should handle successful login and return JWT for user', async () => {
+
+    axios.post.mockResolvedValue({
+      data: {
+        jwt: 'mockedJWT',
+      },
+    });
+
+    const setItemMock = jest.spyOn(window.localStorage.__proto__, 'setItem');
+
+  
+    const data = {
+      username: 'emilyjs',
+      password: 'password',
+    };
+
+    const result = await AuthenticationService.login(data);
+
+  
+    expect(result).toEqual({
+      jwt: 'mockedJWT',
+    
+    });
+
+    expect(setItemMock).toHaveBeenCalledWith('user', JSON.stringify(result));
+  });
+
+  
+  
+
+
+});
 
 
